e0cfb81e86abd27558fb05d5d70162377eaa78e8
Debby-arch <opiyorosedebrah@gmail.com>
2026-02-25 02:55:57 +0300

    Add Hero component with styling and content
    - Created Hero.jsx component to display a hero section with a title and description.
    - Added Hero.css for styling the hero section with a background image and text formatting.

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+      <div className='hero-text'>
+            <h1>Taste Happiness in Every Scoop</h1>
+            <p>At Scoops, we serve creamy, handcrafted ice cream made to brighten your day. From classic favorites to bold new flavors, there’s a scoop waiting just for you.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/Logo.png'
+
+
+
+
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <img src={logo} alt="" className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>About Us</li>
+        <li>Reviews</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
   )
 }
 
